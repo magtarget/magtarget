a28eb671cf72611a7aca94d645bc75c40b845a73
Jean-Michel <jmt@magtarget.com>
2020-09-10 13:53:13 +0000

    fix field error in view

--- leaderboard/models/models.py
+++ leaderboard/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class leaderboard(models.Model):
      _name = 'leaderboard.leaderboard'
 
-     leaderboard_name = fields.Char(string="Leaderboard Name", required=True)
+     name = fields.Char(string="Leaderboard Name", required=True)
      Leaderboard_priority = fields.Integer()
      jm_test_field = fields.Float(compute="_value_pc", store=True)
      leaderboard_description = fields.Text()
