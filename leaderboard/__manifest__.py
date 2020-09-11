79c5f4ba9b74b3ea8e76512e4647811b1c0783b1
Jean-Michel <jmt@magtarget.com>
2020-09-11 23:23:33 +0000

    Fixed res.partner domain typo

--- leaderboard/__manifest__.py
+++ leaderboard/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "MagTarget Leaderboards",
 
     'summary': """
-        This Module will ultimately create views with the top leaderboards. Edit 2""",
+        This Module will ultimately create views with the top leaderboards.""",
 
     'description': """
         We will start by focusing on the Work Order leaderboards. Then eventually add leaderboards to other areas of Odoo.
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
--- leaderboard/models/models.py
+++ leaderboard/models/models.py
@@ -8,8 +8,8 @@ class leaderboard(models.Model):
     name = fields.Char(string="Leaderboard Name", required=True)
     Leaderboard_priority = fields.Integer()
     leaderboard_description = fields.Text()
-    responsible = fields.Many2one('res.partner', string='Responsible', ondelete='set null', context={}, domain=[('share', '=', False)],)
-# another domain filter to limit to internal users:    user = fields.Many2one('res.partner', string='Responsible', ondelete='set null', context={}, domain=[('employee_ids', '!=', False)],)
+    responsible = fields.Many2one('res.partner', string='Responsible', ondelete='set null', context={}, domain=[["share","=",False]],)
+
 #    jm_test_field = fields.Float(compute="_value_pc", store=True)
      
 #
