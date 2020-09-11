a3475c5bd5cd5519ea1516afbdb9f1758d2c7ba7
Jean-Michel <jmt@magtarget.com>
2020-09-11 21:37:23 +0000

    Load sample data on App install

--- leaderboard/__manifest__.py
+++ leaderboard/__manifest__.py
@@ -26,6 +26,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/leaderboard.leaderboard.csv'
     ],
     # only loaded in demonstration mode
     'demo': [
--- leaderboard/models/models.py
+++ leaderboard/models/models.py
@@ -3,12 +3,13 @@
 from odoo import models, fields, api
 
 class leaderboard(models.Model):
-     _name = 'leaderboard.leaderboard'
+    _name = 'leaderboard.leaderboard'
 
-     name = fields.Char(string="Leaderboard Name", required=True)
-     Leaderboard_priority = fields.Integer()
-     jm_test_field = fields.Float(compute="_value_pc", store=True)
-     leaderboard_description = fields.Text()
+    name = fields.Char(string="Leaderboard Name", required=True)
+    Leaderboard_priority = fields.Integer()
+    leaderboard_description = fields.Text()
+#    jm_test_field = fields.Float(compute="_value_pc", store=True)
+     
 #
 #     @api.depends('value')
 #     def _value_pc(self):
