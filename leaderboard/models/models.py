5e7324f121b5a3d8bc978227e964d3a8296bc279
Jean-Michel <jmt@magtarget.com>
2020-09-11 22:45:23 +0000

    Add the default Odoo Partner relational field to the model

--- leaderboard/models/models.py
+++ leaderboard/models/models.py
@@ -8,6 +8,8 @@ class leaderboard(models.Model):
     name = fields.Char(string="Leaderboard Name", required=True)
     Leaderboard_priority = fields.Integer()
     leaderboard_description = fields.Text()
+    user = fields.Many2one('res.partner', string='Publisher', ondelete='set null', context={}, domain=[],)
+
 #    jm_test_field = fields.Float(compute="_value_pc", store=True)
      
 #
