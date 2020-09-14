6025eb1d3f0f478c866247567b88dbede1f11141
JMT <54421695+magtarget@users.noreply.github.com>
2020-09-14 16:05:25 -0700

    Test adding Modules via GitHub Desktop downloaded from Odoo App Store

---
+++ website_support_gamification/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Website Help Desk / Support Ticket - Gamification",
+    'version': "1.0.0",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'summary': "Adds badges to support tickets",
+    'description': "Adds badges to support tickets",
+    'license':'LGPL-3',
+    'data': [
+        'data/gamification_data.xml',
+    ],
+    'demo': [],
+    'depends': ['website_support_analytic_timesheets', 'gamification'],
+    'images':[
+    ],
+    'installable': True,
+}
