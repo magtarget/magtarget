a117f550f255c2b570a9dedfc26599646ebf04af
brianmagtarget123 <brian@magtarget.com>
2020-09-19 20:58:36 +0000

    v1.1.0 added expiration on todo task

--- addons/todotask/models/models.py
+++ addons/todotask/models/models.py
@@ -3,11 +3,24 @@
 from odoo import models, fields, api
 
 class todotask(models.Model):
-     _name = 'todo.task'
+    _name = 'todo.task'
 
-     name = fields.Char(string=u"Things to do")
+    name = fields.Char(string=u"Things to do")
+    is_done = fields.Boolean(string=u"Done")
+    priority = fields.Selection([('todo', 'Things on hand'),('normal', 'Normal'),('urgent', 'Urgent')], default='todo', string='Urgent Level')
+    deadline = fields.Datetime(string="Deadline")
+    is_expired = fields.Boolean(string="Expired", compute="_compute_is_expired")
+    
+    @api.depends("deadline")
+    @api.multi
+    def _compute_is_expired(self):
+        for rec in self:
+            if rec.deadline:
+                rec.is_expired = rec.deadline < fields.Datetime.now()
+            else:
+                rec.is_expired = False
+    
 
-     is_done = fields.Boolean(string=u"Done")
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
